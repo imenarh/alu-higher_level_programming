95f4fbf2073b26e67b4ef038b20adc5d6973b0a7
Rwigema Herve <h.rwigema@gmail.com>
2025-10-31 06:16:21 -0700

    best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -5,8 +5,9 @@ def best_score(a_dictionary):
     if len(a_dictionary)>0:
         best_value = 0
         best_key = 0
-        for i in a_dictionary.items():
-            if a_dictionary[i] > best_value:
+        for i, value in a_dictionary.items():
+            if value > best_value:
                 best_key = i
+                best_value = value
         return best_key
     return a_dictionary  

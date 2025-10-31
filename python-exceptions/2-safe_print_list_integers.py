803b8ad477a45da5ec2402c4ae5a95e2dab7dafd
Rwigema Herve <h.rwigema@gmail.com>
2025-10-31 09:27:57 -0700

    error fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,9 +7,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
+            print()
         except (TypeError, ValueError):
             pass
         except IndexError:
             break
-    print()
     return count

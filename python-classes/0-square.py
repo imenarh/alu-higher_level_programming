8f469326e308ca7d8c51a8588e98ad79f19e72b2
Herve Rwigema <h.rwigema@alustudent.com>
2025-11-14 21:32:14 +0200

    0 square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Module that defines a Square class."""
+"""This module defines an empty class Square."""
 
 
 class Square:
     """Represents an empty square."""
-pass
+    pass

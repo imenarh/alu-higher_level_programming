87de6c4e24918cc423568137d835fe93dba2bf9d
Herve Rwigema <h.rwigema@alustudent.com>
2026-01-19 06:53:52 -0800

    standards fixed

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -4,11 +4,11 @@ const { argv } = require('node:process');
 
 let num;
 argv.forEach((val, index) => {
-  num=index;	
+  num = index;
 });
 
 if (num <= 1) {
   console.log('No argument');
-}else {
- console.log(argv[2]);
+} else {
+  console.log(argv[2]);
 }

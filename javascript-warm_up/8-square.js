44252023e8de538106c58e36f7cb252c7af9fbdb
Herve Rwigema <h.rwigema@alustudent.com>
2026-01-22 21:03:38 -0800

    printing an x square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const arg = Number(process.argv[2]);
+
+if (isNaN(arg)) {
+	console.log("Missing size");
+	return;
+}
+
+for (let i=0; i<arg; i++) {
+	console.log("x".repeat(arg));
+}

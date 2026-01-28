9030820fee4c572826188819c722707943491bb1
Herve Rwigema <h.rwigema@alustudent.com>
2026-01-28 11:30:07 -0800

    $MSG

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,10 @@
 
 const args = process.argv.slice(2).map(Number);
 
+if (args.length <= 1) {
+  console.log(0);
+  process.exit(0);
+}
 for (let i = 0; i < args.length; i++) {
   for (let j = i + 1; j < args.length; j++) {
     if (args[j] > args[i]) {
